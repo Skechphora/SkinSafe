01f56b236b3f8b478fa045c5e237d4945c1e96e3
ctolmsted <prunes03smugger@icloud.com>
2023-01-23 16:30:08 -0500

    on code review for other push, added normalization and conversion logic in searchedIngredients for allergens state

--- client/Components/SearchedIngredients.jsx
+++ client/Components/SearchedIngredients.jsx
@@ -5,10 +5,12 @@ import { useSelector } from 'react-redux';
 
 const SearchedIngredients = () => {
   // grabs the alergens from state (set on each search in the SearchBar component)
-  const allergens = useSelector((state) => state.products.allergens);
+  let allergens = useSelector((state) => state.products.allergens);
+  allergens = allergens.replaceAll(' ','')
+  const allergenArray = allergens.split(', ');
   // populates an array of one list item per allergen to render 
   const allergenList = [];
-  for (let i = 0; i < allergens.length; i++) {
+  for (let i = 0; i < allergenArray.length; i++) {
     allergenList.push(<li>{allergens[i]}</li>);
   }
   // render all alergens in unordered list
