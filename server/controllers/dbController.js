38c26def823a98be0e7ff343b7b709b9070e5092
Brach Burdick <brach.burdick@gmail.com>
2023-01-26 11:43:46 -0500

    query updated

--- server/controllers/dbController.js
+++ server/controllers/dbController.js
@@ -3,12 +3,10 @@ const pgSql = require('../models/pgSqlDatabase.js')
 
 const dbControllers = {};
 
-
-
 dbControllers.getAllProducts = (req, res, next) =>{
 console.log('inside dbControllers.getAllProducts mw')
 const product_query = `
-SELECT products.* , brands.name AS brand_name 
+SELECT products.* , brands.name AS brand_name, categories.category
 FROM products 
 LEFT JOIN brands ON products.brand_id = brands._id 
 LEFT JOIN categories ON products.category_id = categories._id`
@@ -115,36 +113,7 @@ for (const ele of allProducts) {
 //TODO:
 //Save query response in res.locals.getProduct. Also maybe format it.
 //Get product information from database; - need to check the db next step
-dbControllers.getProduct = (req, res, next) => {
-  console.log('within dbControllers.getProduct mwf, passing in:')
-  console.log(req.body)
-  // //save the info from req from req.body
-  const { allergons } = req.body //this should be a array, send from front end;
-  const params = allergons;
-  //console.log("params", params);
 
-  //this query return product id given string===desired ingredient
-  //get info from request, (one ingredient name), and product query is return products that contians the allergons
-    const product_query = `
-      SELECT DISTINCT pi.sub_product_id 
-      FROM product_ingredient pi 
-      INNER JOIN ingredient_list i 
-      ON pi.ingredient_id=i._id 
-      WHERE i.ingredient=$1
-      `;
-
-    pgSql.query(product_query, params).then((data) => {
-      console.log(data.rows);
-    })
-    .catch((error) => {
-        next({
-          error : {
-            log: `having error getting product from database : ${error}`,
-            message: {error: 'An error ocurred here'}
-          }
-        });
-      });
-}
 
 
 module.exports = dbControllers;
