2e08834ba034ab7af5538621e41ac1daff634a90
stphnl <stphn.l.nyc@gmail.com>
2023-01-26 13:57:00 -0500

    preparing for merge to dev and PR

--- client/Slices/productsSlice.js
+++ client/Slices/productsSlice.js
@@ -25,31 +25,6 @@ export const productsSlice = createSlice({
   },
 });
 
-/* ========== Redux thunk middleware syntax =========== */
-// the outside custom "thunk creator" function, dispatch this in a React component
-export const fetchProductsByAllergen = () => {
-  // the inside "thunk function"
-  // note on useSelector vs getState: https://www.reddit.com/r/react/comments/jdbzme/what_is_the_difference_between_getstate_and/
-  return (dispatch, getState) => {
-    // retrieve our allergens property from our state which at this point,
-    // should be an proper array after going through the 'restrictAllergenInputs' thunk creator
-    const allergens = getState().products.allergens;
-
-    // then use promise-chaining on a fetch API to resolve the response from the server, 
-    // and to update the 'results' property in our state
-    fetch('/api/getAllProducts', {
-      method: "GET",
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      // send a JSON object with the key-value pair of 'allergens'
-      // body: JSON.stringify({ allergens })
-    })
-      .then(response => response.json())
-      .then(response => dispatch(update_results(response)))
-      .catch(err => console.log(err));
-    }
-  }
 
 // Dispatching 'restrictAllergenInputs' within the 'SearchBar' component to limit the number of
 // allergen inputs to only 5 when the submit button is pressed.
@@ -65,7 +40,7 @@ export const restrictAllergenInputs = () => {
     // and to handle any inputs less than 5 allergens by filling in the rest of the array with empty strings
     // to satisfy the DB query requirements
     else {
-      allergens = allergens.replaceAll(' ','').split(',').slice(0,4);
+      allergens = allergens.replaceAll(' ','').toUpperCase().split(',').slice(0,4);
       while (allergens.length < 5) {
         allergens.push('');
       }
@@ -73,6 +48,33 @@ export const restrictAllergenInputs = () => {
     }
   }
 }
+
+
+/* ========== Redux thunk middleware syntax =========== */
+// the outside custom "thunk creator" function, dispatch this in a React component
+export const fetchProductsByAllergen = () => {
+  // the inside "thunk function"
+  // note on useSelector vs getState: https://www.reddit.com/r/react/comments/jdbzme/what_is_the_difference_between_getstate_and/
+  return (dispatch, getState) => {
+    // retrieve our allergens property from our state which at this point,
+    // should be an proper array after going through the 'restrictAllergenInputs' thunk creator
+    const allergens = getState().products.allergens;
+    console.log(allergens);
+    // then use promise-chaining on a fetch API to resolve the response from the server, 
+    // and to update the 'results' property in our state
+    fetch('/api/filterOutAllergens', {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      // send a JSON object with the key-value pair of 'allergens'
+      body: JSON.stringify({ allergens })
+    })
+      .then(response => response.json())
+      .then(response => dispatch(update_results(response)))
+      .catch(err => console.log(err));
+    }
+  }
 
 /** Redux Toolkit createAsyncThunk API syntax */
 export const { update_allergens, update_results } = productsSlice.actions;
